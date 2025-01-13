5649228faa117c2f4e3722c41933a76fe141c418
lsy <taotao313@qq.com>
2025-01-13 16:58:48 +0800

    A:ConcurrentHashMap

--- oracle-jdk8/src/source/java/util/concurrent/ConcurrentHashMap.java
+++ oracle-jdk8/src/source/java/util/concurrent/ConcurrentHashMap.java
@@ -1009,48 +1009,53 @@ public class ConcurrentHashMap<K,V> extends AbstractMap<K,V>
     /** Implementation for put and putIfAbsent */
     final V putVal(K key, V value, boolean onlyIfAbsent) {
         if (key == null || value == null) throw new NullPointerException();
+        // 根据 key 计算出 hashcode
         int hash = spread(key.hashCode());
         int binCount = 0;
-        for (Node<K,V>[] tab = table;;) {
-            Node<K,V> f; int n, i, fh;
-            if (tab == null || (n = tab.length) == 0)
+        // 抛弃了原有的 Segment 分段锁，而采用了 CAS + synchronized 来保证并发安全性
+        for (Node<K, V>[] tab = table; ; ) {
+            Node<K, V> f;
+            int n, i, fh;
+            // 判断是否需要进行初始化。
+            if (tab == null || (n = tab.length) == 0) {
                 tab = initTable();
-            else if ((f = tabAt(tab, i = (n - 1) & hash)) == null) {
-                if (casTabAt(tab, i, null,
-                             new Node<K,V>(hash, key, value, null)))
+            } else if ((f = tabAt(tab, i = (n - 1) & hash)) == null) {
+                // f 即为当前 key 定位出的 Node，如果为空表示当前位置可以写入数据，利用 CAS 尝试写入，失败则自旋保证成功
+                if (casTabAt(tab, i, null, new Node<K, V>(hash, key, value, null)))
                     break;                   // no lock when adding to empty bin
             }
+            // 如果当前位置的 hashcode == MOVED == -1,则需要进行扩容。
             else if ((fh = f.hash) == MOVED)
                 tab = helpTransfer(tab, f);
             else {
                 V oldVal = null;
+                // 如果都不满足，则利用 synchronized 锁写入数据
                 synchronized (f) {
                     if (tabAt(tab, i) == f) {
                         if (fh >= 0) {
                             binCount = 1;
-                            for (Node<K,V> e = f;; ++binCount) {
+                            for (Node<K, V> e = f; ; ++binCount) {
                                 K ek;
                                 if (e.hash == hash &&
-                                    ((ek = e.key) == key ||
-                                     (ek != null && key.equals(ek)))) {
+                                        ((ek = e.key) == key ||
+                                                (ek != null && key.equals(ek)))) {
                                     oldVal = e.val;
                                     if (!onlyIfAbsent)
                                         e.val = value;
                                     break;
                                 }
-                                Node<K,V> pred = e;
+                                Node<K, V> pred = e;
                                 if ((e = e.next) == null) {
-                                    pred.next = new Node<K,V>(hash, key,
-                                                              value, null);
+                                    pred.next = new Node<K, V>(hash, key,
+                                            value, null);
                                     break;
                                 }
                             }
-                        }
-                        else if (f instanceof TreeBin) {
-                            Node<K,V> p;
+                        } else if (f instanceof TreeBin) {
+                            Node<K, V> p;
                             binCount = 2;
-                            if ((p = ((TreeBin<K,V>)f).putTreeVal(hash, key,
-                                                           value)) != null) {
+                            if ((p = ((TreeBin<K, V>) f).putTreeVal(hash, key,
+                                    value)) != null) {
                                 oldVal = p.val;
                                 if (!onlyIfAbsent)
                                     p.val = value;
@@ -1059,6 +1064,7 @@ public class ConcurrentHashMap<K,V> extends AbstractMap<K,V>
                     }
                 }
                 if (binCount != 0) {
+                    // 如果数量大于 TREEIFY_THRESHOLD 则要转换为红黑树。
                     if (binCount >= TREEIFY_THRESHOLD)
                         treeifyBin(tab, i);
                     if (oldVal != null)
