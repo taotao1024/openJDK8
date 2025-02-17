bb1a03ee22d23959e64cd996f036e97730039b9f
lsy <taotao313@qq.com>
2025-02-17 09:51:37 +0800

    A:运算符 +、++

--- taotao-jvm/src/main/java/com/taotao/jvm/example/operation/Add.java
+++ taotao-jvm/src/main/java/com/taotao/jvm/example/operation/Add.java
@@ -3,30 +3,24 @@ package com.taotao.jvm.example.operation;
 public class Add {
 
     public static void main(String[] args) {
-//        int a = 1;
-//        int b = 2;
-//
-//        System.out.println(a + b);
-
-        //=====
-//        float a = 1;
-//        float b = 2;
-//
-//        System.out.println(a + b);
-
-        //=====
-//        long a = 1;
-//        long b = 2;
-//
-//        System.out.println(a + b);
-
-        //=====
-        double a = 1;
-        double b = 2;
-
-        System.out.println(a + b);
+        addInt();
+        addFloat();
+        addLong();
+        addDouble();
     }
 
+    /**
+     *  0 iconst_1
+     *  1 istore_0
+     *  2 iconst_2
+     *  3 istore_1
+     *  4 getstatic #9 <java/lang/System.out : Ljava/io/PrintStream;>
+     *  7 iload_0
+     *  8 iload_1
+     *  9 iadd
+     * 10 invokevirtual #21 <java/io/PrintStream.println : (I)V>
+     * 13 return
+     */
     public static void addInt() {
         int a = 1;
         int b = 2;
@@ -34,6 +28,18 @@ public class Add {
         System.out.println(a + b);
     }
 
+    /**
+     *  0 fconst_1
+     *  1 fstore_0
+     *  2 fconst_2
+     *  3 fstore_1
+     *  4 getstatic #9 <java/lang/System.out : Ljava/io/PrintStream;>
+     *  7 fload_0
+     *  8 fload_1
+     *  9 fadd
+     * 10 invokevirtual #24 <java/io/PrintStream.println : (F)V>
+     * 13 return
+     */
     public static void addFloat() {
         float a = 1;
         float b = 2;
@@ -41,6 +47,18 @@ public class Add {
         System.out.println(a + b);
     }
 
+    /**
+     *  0 lconst_1
+     *  1 lstore_0
+     *  2 ldc2_w #27 <2>
+     *  5 lstore_2
+     *  6 getstatic #9 <java/lang/System.out : Ljava/io/PrintStream;>
+     *  9 lload_0
+     * 10 lload_2
+     * 11 ladd
+     * 12 invokevirtual #29 <java/io/PrintStream.println : (J)V>
+     * 15 return
+     */
     public static void addLong() {
         long a = 1;
         long b = 2;
@@ -48,6 +66,18 @@ public class Add {
         System.out.println(a + b);
     }
 
+    /**
+     *  0 dconst_1
+     *  1 dstore_0
+     *  2 ldc2_w #7 <2.0>
+     *  5 dstore_2
+     *  6 getstatic #9 <java/lang/System.out : Ljava/io/PrintStream;>
+     *  9 dload_0
+     * 10 dload_2
+     * 11 dadd
+     * 12 invokevirtual #15 <java/io/PrintStream.println : (D)V>
+     * 15 return
+     */
     public static void addDouble() {
         double a = 1;
         double b = 2;
--- taotao-jvm/src/main/java/com/taotao/jvm/example/operation/AddAdd.java
+++ taotao-jvm/src/main/java/com/taotao/jvm/example/operation/AddAdd.java
@@ -3,62 +3,147 @@ package com.taotao.jvm.example.operation;
 public class AddAdd {
 
     public static void main(String[] args) {
-//        int v = 1;
-//        System.out.println(v++);
-//        System.out.println(++v);
-
-        //=====
-//        byte v = 1;
-//        System.out.println(v++);
-//        System.out.println(++v);
-
-        //=====
-//        float v = 1;
-//        System.out.println(v++);
-//        System.out.println(++v);
-
-        //=====
-//        long v = 1;
-//        System.out.println(v++);
-//        System.out.println(++v);
-
-        //=====
-        double v = 1;
-        System.out.println(v++);
-        System.out.println(++v);
+        intAdd();
+        byteAdd();
+        floatAdd();
+        longAdd();
+        doubleAdd();
+        doubleAdd2();
     }
 
+    /**
+     *  0 iconst_1
+     *  1 istore_0
+     *  2 getstatic #27 <java/lang/System.out : Ljava/io/PrintStream;>
+     *  5 iload_0
+     *  6 iinc 0 by 1
+     *  9 invokevirtual #33 <java/io/PrintStream.println : (I)V>
+     * 12 getstatic #27 <java/lang/System.out : Ljava/io/PrintStream;>
+     * 15 iinc 0 by 1
+     * 18 iload_0
+     * 19 invokevirtual #33 <java/io/PrintStream.println : (I)V>
+     * 22 return
+     */
     public static void intAdd() {
         int v = 1;
         System.out.println(v++);
         System.out.println(++v);
     }
 
-    //=====
+    /**
+     *  0 iconst_1
+     *  1 istore_0
+     *  2 getstatic #27 <java/lang/System.out : Ljava/io/PrintStream;>
+     *  5 iload_0
+     *  6 iload_0
+     *  7 iconst_1
+     *  8 iadd
+     *  9 i2b
+     * 10 istore_0
+     * 11 invokevirtual #33 <java/io/PrintStream.println : (I)V>
+     * 14 getstatic #27 <java/lang/System.out : Ljava/io/PrintStream;>
+     * 17 iload_0
+     * 18 iconst_1
+     * 19 iadd
+     * 20 i2b
+     * 21 istore_0
+     * 22 iload_0
+     * 23 invokevirtual #33 <java/io/PrintStream.println : (I)V>
+     * 26 return
+     */
     public static void byteAdd() {
         byte v = 1;
         System.out.println(v++);
         System.out.println(++v);
     }
 
+    /**
+     *  0 fconst_1
+     *  1 fstore_0
+     *  2 getstatic #27 <java/lang/System.out : Ljava/io/PrintStream;>
+     *  5 fload_0
+     *  6 dup
+     *  7 fconst_1
+     *  8 fadd
+     *  9 fstore_0
+     * 10 invokevirtual #39 <java/io/PrintStream.println : (F)V>
+     * 13 getstatic #27 <java/lang/System.out : Ljava/io/PrintStream;>
+     * 16 fload_0
+     * 17 fconst_1
+     * 18 fadd
+     * 19 dup
+     * 20 fstore_0
+     * 21 invokevirtual #39 <java/io/PrintStream.println : (F)V>
+     * 24 return
+     */
     public static void floatAdd() {
         float v = 1;
         System.out.println(v++);
         System.out.println(++v);
     }
 
+    /**
+     *  0 lconst_1
+     *  1 lstore_0
+     *  2 getstatic #27 <java/lang/System.out : Ljava/io/PrintStream;>
+     *  5 lload_0
+     *  6 dup2
+     *  7 lconst_1
+     *  8 ladd
+     *  9 lstore_0
+     * 10 invokevirtual #42 <java/io/PrintStream.println : (J)V>
+     * 13 getstatic #27 <java/lang/System.out : Ljava/io/PrintStream;>
+     * 16 lload_0
+     * 17 lconst_1
+     * 18 ladd
+     * 19 dup2
+     * 20 lstore_0
+     * 21 invokevirtual #42 <java/io/PrintStream.println : (J)V>
+     * 24 return
+     */
     public static void longAdd() {
         long v = 1;
         System.out.println(v++);
         System.out.println(++v);
     }
 
+    /**
+     *  0 dconst_1
+     *  1 dstore_0
+     *  2 getstatic #27 <java/lang/System.out : Ljava/io/PrintStream;>
+     *  5 dload_0
+     *  6 dup2
+     *  7 dconst_1
+     *  8 dadd
+     *  9 dstore_0
+     * 10 invokevirtual #45 <java/io/PrintStream.println : (D)V>
+     * 13 getstatic #27 <java/lang/System.out : Ljava/io/PrintStream;>
+     * 16 dload_0
+     * 17 dconst_1
+     * 18 dadd
+     * 19 dup2
+     * 20 dstore_0
+     * 21 invokevirtual #45 <java/io/PrintStream.println : (D)V>
+     * 24 return
+     */
     public static void doubleAdd() {
         double v = 1;
         System.out.println(v++);
         System.out.println(++v);
     }
 
+    /**
+     *  0 dconst_1
+     *  1 dstore_0
+     *  2 getstatic #27 <java/lang/System.out : Ljava/io/PrintStream;>
+     *  5 dload_0
+     *  6 dconst_1
+     *  7 dadd
+     *  8 dup2
+     *  9 dstore_0
+     * 10 invokevirtual #45 <java/io/PrintStream.println : (D)V>
+     * 13 return
+     */
     public static void doubleAdd2() {
         double v = 1;
         System.out.println(++v);
